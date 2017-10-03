df8754f285e26215c25b1b48affeae5a31afaf09
sintanial <sintanial@gmail.com>
2017-10-03 18:06:28 +0300

    first commit

---
+++ lerry.go
@@ -0,0 +1,71 @@
+package lerry
+
+import (
+	"reflect"
+	"github.com/ansel1/merry"
+	"github.com/mgutz/logxi/v1"
+)
+
+func Wrap(e error, level int) merry.Error {
+	if e == nil {
+		return nil
+	}
+
+	return merry.WrapSkipping(e, 1).WithValue("level", level)
+}
+
+func AlertWrap(e error) merry.Error {
+	return Wrap(e, log.LevelAlert)
+}
+
+func FatalWrap(e error) merry.Error {
+	return Wrap(e, log.LevelFatal)
+}
+func ErrorWrap(e error) merry.Error {
+	return Wrap(e, log.LevelError)
+}
+func WarnWrap(e error) merry.Error {
+	return Wrap(e, log.LevelWarn)
+}
+
+func NoticeWrap(e error) merry.Error {
+	return Wrap(e, log.LevelNotice)
+}
+
+func InfoWrap(e error) merry.Error {
+	return Wrap(e, log.LevelInfo)
+}
+
+func Print(lg log.Logger, err error) {
+	level, msg, args := Prepare(err)
+	lg.Log(level, msg, args)
+}
+
+func Prepare(err error) (level int, message string, arguments []interface{}) {
+	values := merry.Values(err)
+
+	args := make([]interface{}, 0)
+
+	for key, val := range values {
+		if key == "user message" {
+			continue
+		}
+
+		if reflect.TypeOf(key).String() == "string" {
+			args = append(args, key, val)
+		}
+	}
+
+	if strace := merry.Stacktrace(err); strace != "" {
+		args = append(args, "stack", strace)
+	}
+
+	errmsg := err.Error()
+	usrmsg := merry.UserMessage(err)
+	lv, ok := merry.Value(err, "level").(int)
+	if !ok {
+		lv = log.LevelWarn
+	}
+
+	return lv, usrmsg + ": " + errmsg, args
+}
